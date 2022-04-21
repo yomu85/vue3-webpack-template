2f03202145756c565535c8466084c1562e38371d
yomu85 <yomu85@gmail.com>
2022-04-21 18:12:49 +0900

    create project

---
+++ src/components/Fruit.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
